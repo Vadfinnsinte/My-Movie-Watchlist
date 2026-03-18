f3f21ee339f4eaad7fab9110daec721a92bc1245
Linda Westerlund <lindawesterlund94@gmail.com>
2026-03-18 16:12:01 +0100

    initial setup

---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import "../style/header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <p>My Movies</p>
+      <p>Home</p>
+      <p>User</p>
+    </div>
+  );
+};
+
+export default Header;
